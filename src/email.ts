9bc723ae27e8bfa0109d51fad30eeb1a399cc556
Kunal Bagaria <61944452+kb24x7@users.noreply.github.com>
2021-12-28 00:23:46 +0530

    removed spacing

--- src/email.ts
+++ src/email.ts
@@ -5,7 +5,7 @@ const template = (accepted: boolean): string => (
 Hello,
 
 Thanks for applying for a Superteam Instagrant. ${accepted ? 'Happy to inform you that the grant application has been accepted, and you will receive the amount shortly. Until then, please fill in the grant onboarding form available at - https://airtable.com/shrR1FTUDMCoGFoSY.' : 'However, we regret to inform you that your project is not currently a good fit for us right now. We hope you apply again down the line.'}
-${accepted ? '\nDM me at @ neilshroff#2180 for your role to be changed to a Member (if not already a member) and for any other queries. \n' : ''}
+${accepted ? '\nDM me at @neilshroff#2180 for your role to be changed to a Member (if not already a member) and for any other queries. \n' : ''}
 Best,
 Neil from Superteam
 `
